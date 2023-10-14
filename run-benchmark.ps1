6b17bc28655ddabb137af13094545ecf98997240
hdrie <36448039+hdrie@users.noreply.github.com>
2023-10-14 16:48:23 +0200

    Added intended script structure

--- run-benchmark.ps1
+++ run-benchmark.ps1
@@ -1,1 +1,20 @@
+# X-Plane Benchmark - Automated PowerShell script
+# X-Plane 12 only!
+#
+# CONFIGURATION / PARAMETERS
+#   Input/Output files
+#   Benchmark options
 
+
+
+# FUNCTION DEFINITIONS
+#   Start executable
+#   Read system config
+#   Read benchmark results from Log.txt
+#   Write/append output csv file
+
+
+
+# SCRIPT EXECUTION
+#   Loop benchmark runs as configured
+#   Read and write system config and benchmark results
