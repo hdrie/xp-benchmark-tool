d9d59be68edcde4ba5ab9c3ede1b3f9e4f0226fe
hdrie <36448039+hdrie@users.noreply.github.com>
2023-10-23 22:24:03 +0200

    Implement process launching
    assign global "replayFile benchCode args" variables
    implement  "startBench" function

--- run-benchmark.ps1
+++ run-benchmark.ps1
@@ -4,7 +4,9 @@
 # CONFIGURATION / PARAMETERS
 #   Input/Output files
 #   Benchmark options
-
+$replayFile="Output/replays/test_flight_737.fps"
+$benchCode="1"
+$args="--fps_test=$benchCode --load_smo=$replayFile"
 
 
 # FUNCTION DEFINITIONS
@@ -12,9 +14,11 @@
 #   Read system config
 #   Read benchmark results from Log.txt
 #   Write/append output csv file
+function startBench {Start-Process -FilePath "./x-plane.exe" -ArgumentList $args}
 
 
 
 # SCRIPT EXECUTION
 #   Loop benchmark runs as configured
 #   Read and write system config and benchmark results
+startBench
