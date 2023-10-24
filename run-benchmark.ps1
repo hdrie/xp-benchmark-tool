bab7a0f1d1318a2a548bc0807599637bca5dfe31
hdrie <36448039+hdrie@users.noreply.github.com>
2023-10-24 13:37:07 +0200

    bench code iteration,  universal runExec function
    Implement iteration through bench codes and make runExec function universal

--- run-benchmark.ps1
+++ run-benchmark.ps1
@@ -4,21 +4,29 @@
 # CONFIGURATION / PARAMETERS
 #   Input/Output files
 #   Benchmark options
-$replayFile="Output/replays/test_flight_737.fps"
-$benchCode="1"
-$args="--fps_test=$benchCode --load_smo=$replayFile"
-
+$execPath="x-plane.exe"
+$replayPath="Output/replays/test_flight_737.fps"
+$logPath="Log.txt"
+$benchCodes=1,2,3,4,5
 
 # FUNCTION DEFINITIONS
 #   Start executable
 #   Read system config
 #   Read benchmark results from Log.txt
 #   Write/append output csv file
-function startBench {Start-Process -FilePath "./x-plane.exe" -ArgumentList $args}
-
+function runExec {
+    param (
+        $exec
+        $args
+    )
+    Start-Process -FilePath $exec -ArgumentList $args -Wait
+}
 
 
 # SCRIPT EXECUTION
 #   Loop benchmark runs as configured
 #   Read and write system config and benchmark results
-startBench
+
+foreach ($code in $benchCodes) {
+    runExec -exec $execPath -args "‐‐fps_test=$code ‐‐load_smo=$replayPath"
+}
