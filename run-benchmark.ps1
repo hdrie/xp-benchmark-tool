a3fa483cb648bce0090739087a7ea840d10e86d0
hdrie <36448039+hdrie@users.noreply.github.com>
2023-10-25 23:55:26 +0200

    added features
    - obtain benchmark results
    - obtain xp version
    - improved results file formatting

--- run-benchmark.ps1
+++ run-benchmark.ps1
@@ -8,7 +8,8 @@ $execPath='./x-plane.exe'
 $replayPath='Output/replays/test_flight_737.fps'
 $logPath='./Log.txt'
 $resultsPath='./ZZ_BenchResults.txt'
-$benchCodes=1,2,3,4,5
+$benchCodes=41
+$launchArguments="--weather_seed=1 --time_seed=1"
 
 # FUNCTION DEFINITIONS
 
@@ -23,13 +24,13 @@ function runExec {
     Start-Process -FilePath $exec -ArgumentList $args -Wait
 }
 
-#   Read benchmark results from Log.txt, does nothing yet
+#   Read benchmark results from Log.txt
 function getResults {
     param (
         [Parameter(Mandatory=$true, Position=0)]
         [string] $logFile
     )
-    return "ExampleResults"
+    return (Get-Content $logFile | Select-String -Pattern 'FRAMERATE TEST:','GPU LOAD:' | Out-String).trim()
 }
 
 #   Read hardware config, does nothing yet
@@ -41,13 +42,13 @@ function getHWconf {
     return "ExampleHWconf"
 }
 
-#   Read software config, does nothing yet
+#   Read software config, only X-Plane version atm
 function getSWconf {
     param (
         [Parameter(Mandatory=$true, Position=0)]
         [string] $logFile
     )
-    return "ExampleSWconf"
+    return (Get-Content Log.txt -First 1).Split(' ')[3]
 }
 
 # SCRIPT EXECUTION
@@ -59,12 +60,15 @@ $readConfig=$true
 
 # Create results file if not exists
 if (!(Test-Path $resultsPath -PathType Leaf)) {
-    New-Item ./ZZ_BenchResults.txt -ItemType File -Value "This is the results file"
+    New-Item ./ZZ_BenchResults.txt -ItemType File
 }
+
+# Create header line
+Add-Content -Path $resultsPath -Value "BEGIN SESSION: $(Get-Date -UFormat "%m/%d/%Y %R")"
 
 # Run the benchmarks, looping through the specified bench codes
 foreach ($code in $benchCodes) {
-    runExec -exec $execPath -args "--fps_test=$code --load_smo=$replayPath"
+    runExec -exec $execPath -args "--fps_test=$code --load_smo=$replayPath $launchArguments"
     if ($readConfig) {
         # Read and write the system configuration once every session
         $HWconfig = getHWconf -logFile $logPath
@@ -72,8 +76,11 @@ foreach ($code in $benchCodes) {
         Add-Content -Path $resultsPath -Value "$HWconfig`n$SWconfig"
         $readConfig=$false
     }
-    # Get results from Log.txt
+    # Get results from Log.txt, specifying benchmark code
+    Add-Content -Path $resultsPath -Value "`nBenchmark Code: $code" 
     $results = getResults -logFile $logPath
     # Write config and results to File
     Add-Content -Path $resultsPath -Value $results
 }
+
+Add-Content -Path $resultsPath -Value "END SESSION: $(Get-Date -UFormat "%m/%d/%Y %R")`n-------------------------"
