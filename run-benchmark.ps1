d2df2ba84680e39a9cfc30da5da9a83b910f2c50
hdrie <h.driehaus@gmail.com>
2023-11-07 12:05:56 +0100

    rewrite/merge sys config func , better var names

--- run-benchmark.ps1
+++ run-benchmark.ps1
@@ -1,15 +1,24 @@
 # X-Plane Benchmark - Automated PowerShell script
 # X-Plane 12 only!
-#
+
+# -------------------------------------------------------------------------------------------
+# IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII
+# -------------------------------------------------------------------------------------------
+
 # CONFIGURATION / PARAMETERS
 #   Input/Output files
 #   Benchmark options
 $execPath='./x-plane.exe'
 $replayPath='Output/replays/test_flight_737.fps'
 $logPath='./Log.txt'
-$resultsPath='./ZZ_BenchResults.txt'
+$txtOutPath='./ZZ_BenchResults.txt'
+$launchArguments="--load_smo=$replayPath --weather_seed=1 --time_seed=1"
+
 $benchCodes=1,3,5,41,43,45
-$launchArguments="--weather_seed=1 --time_seed=1"
+
+# -------------------------------------------------------------------------------------------
+# IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII
+# -------------------------------------------------------------------------------------------
 
 # FUNCTION DEFINITIONS
 
@@ -33,34 +42,36 @@ function getResults {
     return (Get-Content $logFile | Select-String -Pattern 'FRAMERATE TEST:','GPU LOAD:' | Out-String).trim()
 }
 
-#   Read hardware config from Log.txt, returns a string
-function getHWconf {
+#   Read System Configuration
+function getSysConf {
     param (
         [Parameter(Mandatory=$true, Position=0)]
         [string] $logFile
     )
-    # $cpu=$(Get-CimInstance -ClassName Win32_Processor)[0].Name # We could also get the cpu name from the system, instead of Log.txt
+    # Software
+    #   X-Plane Version
+    $xpVer=$(Get-Content $logFile -First 1).Split(' ')[3]
+    #   Zink setting
+    $zinkOn=$(Get-Content $logFile | Select-String -Pattern 'OpenGL Render' | Out-String) -match 'zink'
+    
+    # Hardware
+    #   CPU Device
+    #$cpu=$(Get-CimInstance -ClassName Win32_Processor)[0].Name # We could also get the cpu name from the system, instead of Log.txt
     (Get-Content $logFile | Out-String) -match 'CPU 0: (.+)\s+Speed.+' | out-null
     $cpu=$matches[1]
+    #   GPU Device
     (Get-Content $logFile | Out-String) -match 'Vulkan Device\s+: (.+)' | out-null
     $gpu=$matches[1]
+    #   RAM amount
     (Get-Content $logFile | Out-String) -match 'Physical Memory \(total for computer\): (\d+)' | out-null
     $ram=([float]$matches[1]/(1024*1024*1024)).tostring("#.#")
-    return "CPU: $cpu`nGPU: $gpu`nSystem Memory: $ram GB"
+
+    return "X-Plane Version: $xpVer`nZink: $zinkOn`nCPU: $cpu`nGPU: $gpu`nRAM: $ram GB"
 }
 
-#   Read software config:
-#   - X-Plane Version
-#   - Zink setting
-function getSWconf {
-    param (
-        [Parameter(Mandatory=$true, Position=0)]
-        [string] $logFile
-    )
-    $xpVer=$(Get-Content $logFile -First 1).Split(' ')[3]
-    $zinkOn=$(Get-Content $logFile | Select-String -Pattern 'OpenGL Render' | Out-String) -match 'zink'
-    return "X-Plane Version: $xpVer`nZink: $zinkOn"
-}
+# -------------------------------------------------------------------------------------------
+# IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII
+# -------------------------------------------------------------------------------------------
 
 # SCRIPT EXECUTION
 #   Loop benchmark runs as configured
@@ -69,29 +80,29 @@ function getSWconf {
 # Read the system configuration once every session
 $readConfig=$true
 
-# Create results file if not exists
-if (!(Test-Path $resultsPath -PathType Leaf)) {
-    New-Item ./ZZ_BenchResults.txt -ItemType File
+# Create results .txt file if not exists
+if (!(Test-Path $txtOutPath -PathType Leaf)) {
+    New-Item ./ZZ_BenchResults.txt -ItemType File | out-null
 }
 
 # Create header line
-Add-Content -Path $resultsPath -Value "BEGIN SESSION: $(Get-Date -UFormat "%m/%d/%Y %R")"
+Add-Content -Path $txtOutPath -Value "-------------------------`nBEGIN SESSION: $(Get-Date -UFormat "%m/%d/%Y %R")`n-------------------------"
+# Switch to read the system configuration once every session (switch will turn false after first bench run)
+$readConfig=$true
 
 # Run the benchmarks, looping through the specified bench codes
 foreach ($code in $benchCodes) {
-    runExec -exec $execPath -args "--fps_test=$code --load_smo=$replayPath $launchArguments"
+    runExec -exec $execPath -args "--fps_test=$code $launchArguments"
     if ($readConfig) {
         # Read and write the system configuration once every session
-        $HWconfig = getHWconf -logFile $logPath
-        $SWconfig = getSWconf -logFile $logPath
-        Add-Content -Path $resultsPath -Value "$HWconfig`n$SWconfig"
+        $sysConfig = getSysConf -logFile $logPath
+        Add-Content -Path $txtOutPath -Value $sysConfig
         $readConfig=$false
     }
     # Get results from Log.txt, specifying benchmark code
-    Add-Content -Path $resultsPath -Value "`nBenchmark Code: $code" 
+    Add-Content -Path $txtOutPath -Value "`nBenchmark Code: $code" 
     $results = getResults -logFile $logPath
     # Write config and results to File
-    Add-Content -Path $resultsPath -Value $results
+    Add-Content -Path $txtOutPath -Value $results
 }
-
-Add-Content -Path $resultsPath -Value "END SESSION: $(Get-Date -UFormat "%m/%d/%Y %R")`n-------------------------"
+Add-Content -Path $txtOutPath -Value "-------------------------`nEND SESSION: $(Get-Date -UFormat "%m/%d/%Y %R")"
